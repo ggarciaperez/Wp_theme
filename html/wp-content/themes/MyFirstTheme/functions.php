231054d3088a3154ba8e1ea96a5432b96bdd5677
Guillermo Garcia Perez <ggarciaperez@danielcastelao.org>
2020-11-24 17:57:30 +0100

    404 added

--- html/wp-content/themes/MyFirstTheme/functions.php
+++ html/wp-content/themes/MyFirstTheme/functions.php
@@ -6,4 +6,15 @@
 	 */
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size(150,150);
+/**
+ * Filter the except length to 5 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ *//*
+function wpdocs_custom_excerpt_length( $length ) {
+    return 5;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+*/
 ?>
